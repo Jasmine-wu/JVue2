a2f3858f26b9131b6ceca5fe9bf6cda830bac8d1
jasmine <18674601207@163.com>
2023-03-06 14:48:56 +0800

    实现对数组的劫持

--- 01vue-render/src/observe/index.js
+++ 01vue-render/src/observe/index.js
@@ -1,11 +1,41 @@
+import { newArrayProto } from "./array";
+
 class Observer {
   constructor(data) {
+    // 将observer实例绑定到data的自定义属性__obj__上
+    // data.__ob__ = this; // bug:不能直接赋值,会有死循环问题
+    // 解决：定义data的__ob__是不可以枚举的
+    Object.defineProperty(data, "__obj__", {
+      value: this,
+      enumerable: false, //将__ob__ 变成不可枚举 （循环的时候无法获取到）
+    });
+
     // 依次对数据进行劫持
-    this.walk(data);
+    if (Array.isArray(data)) {
+      // 1.如果数组成员是通过数组方法修改的：vm.c.push("dddd");
+      // 则劫持7个数组方法 -> 函数劫持
+      // data.__proto__ = {
+      //   push() {
+      //     console.log("xxxx");
+      //   },
+      // }; // 这样写，会直接覆盖掉数组的原方法
+      // 正确做法：保留数组的原方法同时添加新的东西
+      data.__proto__ = newArrayProto;
+
+      // 2.如果数组成员是通过这样修改 vm.c.five = "fiviviviiv";
+      // 则劫持数组里的引用类型
+      this.observeArray(data);
+    } else {
+      this.walk(data);
+    }
   }
 
   walk(data) {
     Object.keys(data).forEach((key) => defineReactive(data, key, data[key]));
+  }
+
+  observeArray(data) {
+    data.forEach((item) => observe(item));
   }
 }
 
@@ -22,7 +52,7 @@ export function defineReactive(target, key, value) {
       console.log(`${key}属性值被设置了：${newValue}`);
 
       if (newValue === value) return;
-      // 如果你设置了一个对象值
+      // 如果你设置的值是对象，需要对这个对象也进行劫持
       observe(newValue);
       value = newValue;
     },
@@ -33,5 +63,11 @@ export function observe(data) {
   if (typeof data !== "object" || data == null) {
     return;
   }
+
+  // 判断data是否已经被劫持
+  if (data.__obj__ instanceof Observer) {
+    return data.__obj__;
+  }
+
   return new Observer(data);
 }
--- 01vue-render/src/state.js
+++ 01vue-render/src/state.js
@@ -20,7 +20,7 @@ function initData(vm) {
   vm._data = data;
   // vue2 Vue实例可以是对象也可以是函数，组件实例必须是函数
   // vue3 Vue实例必须是函数
-  data = typeof data === "function" ? data.call(vm) : data; // data是用户返回的对象
+  data = typeof data === "function" ? data.call(vm) : data; 
 
   observe(data);
 
